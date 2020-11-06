23e34da7b71d8955310814ae7f5522afebf2aa35
Denis Moreira <denis_narcizo@hotmail.com>
2020-11-06 12:12:57 -0300

    Correção
    Correção do envio da tela vendas com conexão ao banco de dados

--- Index.php
+++ Index.php
@@ -1,11 +1,31 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Full Stack Eletro</title>
+        <link rel="stylesheet" href="CSS/estilo.css">
+
+    </head>
+
+    <body>
+        <?php
+            include('menu.html');
+        ?>
+        <main>
+            <h1>
+                Seja bem vindo(a)!
+            </h1>
+            <p>Aqui em nossa loja, programadores tem desconto nos produtos da casa!</p>
+        </main>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br>
+        <footer id="rodape">
+            <p id="formas_pagamento"><b>Formas de pagamentos:</b></p>
+            <img src="./imagens/formas-pagamentos.png">
+            <p>&copy; Recode Pro</p>
+        </footer>
+    </body>
+
+</html>
